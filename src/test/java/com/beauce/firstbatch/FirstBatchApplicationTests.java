dae95bb900fc0d5a2a20dbe93933f8fb1cb98319
Johan BEAUCE <jbeauce@pictet-technologies.com>
2020-09-06 22:46:33 +0200

    my first batch connected to the DB

---
+++ src/test/java/com/beauce/firstbatch/FirstBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beauce.firstbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
